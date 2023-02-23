5c82c49fffd01e36daa21f55f52389c07913ad26
Walaa Khader <wkhader@ad.wcpss.net>
2023-02-23 13:59:10 -0500

    final push

---
+++ src/vweets.js
@@ -0,0 +1,142 @@
+export default [
+  {
+    _id: '0',
+    author: {
+      profile_img_url:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYyLU4TlT6Hq9rvOMS2fVprU04weTHYRUshvHCSHBnFef3eV3HYKGVn41m0M3YsM7GdTc&usqp=CAU',
+      display_name: 'Tamara Kern',
+      username: '@Tamara_Kern',
+    },
+    body: `What do you think is the best trait to have as a developer?`,
+    stats: {
+      likes: ['user_1', 'user_2', 'user_3', 'user_4', 'user_5'],
+      retweets: ['user_1', 'user_2', 'user_3'],
+      comments: [
+        {
+          username: '@user_1',
+          body: 'Persistence',
+        },
+        {
+          username: '@user_2',
+          body: 'Consistency',
+        },
+        {
+          username: '@user_3',
+          body: 'Patience',
+        },
+        {
+          username: '@user_4',
+          body: 'Self-discipline and continuity.',
+        },
+      ],
+    },
+  },
+  {
+    _id: '1',
+    author: {
+      profile_img_url:
+        'https://icon-library.com/images/profile-pic-icon/profile-pic-icon-28.jpg',
+      display_name: 'Christine Aber',
+      username: '@christineaber',
+    },
+    body: `
+					What specific technology, language, framework, etc do you hope to get into and learn in 2023?
+				`,
+    stats: {
+      likes: ['user_1', 'user_2', 'user_3', 'user_4', 'user_5', 'user_6'],
+      retweets: ['user_1', 'user_2', 'user_3'],
+      comments: [
+        {
+          username: '@user_1',
+          body: 'Persistence',
+        },
+        {
+          username: '@user_2',
+          body: 'Consistency',
+        },
+        {
+          username: '@user_3',
+          body: 'Patience',
+        },
+      ],
+    },
+  },
+  {
+    _id: '2',
+    author: {
+      profile_img_url:
+        'https://icon-library.com/images/business-man-icon/business-man-icon-1.jpg',
+      display_name: 'Thomas Allen',
+      username: '@coderpop10',
+    },
+    body: `One example on how you can enhance your coding skills:
+				<br><br>
+				1. Start by getting into the habit of making small projects with a new language by learning with your favorite coding tutor. 
+				<br><br>
+				2. Once you are comfortable with that you can create your own personal project with it.
+				<br><br>
+				3. Share your knowledge with others by showing it off!`,
+    stats: {
+      likes: ['user_1', 'user_2', 'user_3', 'user_4', 'user_5'],
+      retweets: ['user_1', 'user_2', 'user_3'],
+      comments: [
+        {
+          username: '@user_1',
+          body: 'Persistence',
+        },
+        {
+          username: '@user_2',
+          body: 'Consistency',
+        },
+        {
+          username: '@user_3',
+          body: 'Patience',
+        },
+        {
+          username: '@user_4',
+          body: 'Self-discipline and continuity.',
+        },
+      ],
+    },
+  },
+  {
+    _id: '3',
+    author: {
+      profile_img_url:
+        'https://icon-library.com/images/young-person-icon/young-person-icon-6.jpg',
+      display_name: 'Tyler Potts',
+      username: '@Tyler_Potts_',
+    },
+    body: `
+					If I build a TWITTER clone for my channel, what tech should I use?
+					<br><br>
+    Leave your ideas below!
+				`,
+    image: {
+      url: 'https://pbs.twimg.com/media/Eq1stQeXYAITGi_?format=jpg&name=large',
+      alt: 'Image',
+    },
+    stats: {
+      likes: [
+        'user_1',
+        'user_2',
+        'user_3',
+        'user_4',
+        'user_5',
+        'user_6',
+        'user_7',
+      ],
+      retweets: ['user_1', 'user_2', 'user_3', 'user_4'],
+      comments: [
+        {
+          username: '@user_1',
+          body: 'Persistence',
+        },
+        {
+          username: '@user_2',
+          body: 'Consistency',
+        },
+      ],
+    },
+  },
+];
